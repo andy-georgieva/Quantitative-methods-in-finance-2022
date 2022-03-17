002c0e35d5a78a23754ab58c3d57382454ece3ce
andy-georgieva <101338970+andy-georgieva@users.noreply.github.com>
2022-03-17 03:22:05 +0200

    Update Homework20220310.R

--- Homework20220310.R
+++ Homework20220310.R
@@ -1,18 +1,29 @@
 #####Problem 1#####
 # Write a loop which iterates over all the numbers from 1 to 10 and prints
-# them multiplied by 3
+# them multiplied by 3 
+ 
+for(i in 1:5){print (i*3)}
+
 #####Problem 1#####
 
 #####Problem 2#####
 # Write a loop which chooses 10 random numbers, one at a time from a normal
 # distribution (use rnorm and see the help ?rnorm) and prints the number
-# if it is bigger than 1.
+# if it is bigger than 1. 
+
+for(i in rnorm(1:10)) if (i>1) {print (i)}
+
 #####Problem 2#####
 
 #####Problem 3#####
 # What is the probability that out of a group of 6 men and 8 women, if we pick
 # 5 people at random, exactly 3 will be men?
-# Use a for loop, which simulates the picking.
+# Use a for loop, which simulates the picking. 
+  
+  (choose(6,3)*choose(8,2))/choose(14,5) 
+
+P.S: I do not know how it should happen with a for loop :/
+
 #####Problem 3#####
 
 #####Problem 4#####
