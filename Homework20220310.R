0599bfdc9cec7a3df5a9188f52706acd0dbf1e0e
andy-georgieva <101338970+andy-georgieva@users.noreply.github.com>
2022-03-17 03:16:07 +0200

    Update Homework20220310.R

--- Homework20220310.R
+++ Homework20220310.R
@@ -1,12 +1,12 @@
 #####Problem 1#####
 # Write a loop which iterates over all the numbers from 1 to 10 and prints
-# them multiplied by 3
+# them multiplied by 3 
 #####Problem 1#####
 
 #####Problem 2#####
 # Write a loop which chooses 10 random numbers, one at a time from a normal
 # distribution (use rnorm and see the help ?rnorm) and prints the number
-# if it is bigger than 1.
+# if it is bigger than 1. 
 #####Problem 2#####
 
 #####Problem 3#####
